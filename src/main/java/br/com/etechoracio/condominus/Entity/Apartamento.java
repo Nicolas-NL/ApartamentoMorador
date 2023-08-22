11a20cf295dbcc28f472eeadbe214961fde7079c
Nicolas-NL <you@example.com>
2023-08-22 11:01:36 -0300

    Criacao tabela Morador e Apartamento

---
+++ src/main/java/br/com/etechoracio/condominus/Entity/Apartamento.java
@@ -0,0 +1,28 @@
+package br.com.etechoracio.condominus.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@Entity
+@Table(name="Apartamento")
+
+public class Apartamento {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_apartamento")
+    private Long id;
+    @Column(name = "telefone_apartamento")
+    private String telefone;
+    @Column(name = "numero")
+    private String numero;
+    @JoinColumn(name = "id_condominio_apart")
+    @ManyToOne
+    private Condominio condominio;
+
+}
+
---
+++ src/main/java/br/com/etechoracio/condominus/Entity/Morador.java
@@ -0,0 +1,29 @@
+package br.com.etechoracio.condominus.Entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name="Morador")
+public class Morador {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id_morador")
+    private Long id;
+
+    @Column(name = "nome_morador")
+    private String Nome;
+
+    @Column(name = "cpf_morador")
+    private String cpf;
+
+    @Column(name = "telefone_morador")
+    private String telefone;
+
+    @JoinColumn(name = "id_apartamento_morad")
+    @ManyToOne
+    private Apartamento apartamento;
+}
